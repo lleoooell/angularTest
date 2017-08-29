39b354391535ccce08fc9c54664bb70205edf03d
Leo Casagrande <leo@greenberry.io>
2017-08-29 11:05:00 +0200

    ngRoute

--- app/controller/myCtrl.js
+++ app/controller/myCtrl.js
@@ -1,8 +1,6 @@
 app.controller("myCtrl", function($scope, $rootScope) {
 	$rootScope.monTableau = [{nom : "casagrande", prenom:"leo"},{nom : "vador", prenom:"dark"}];
-    $scope.person = {
-       
-    };
+    // $scope.person = {};
     $scope.submitForm = function(){
     	$rootScope.monTableau.push($scope.person);
     	$scope.person = {};
--- app/myApp.js
+++ app/myApp.js
@@ -1,1 +1,21 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(['$routeProvider','$locationProvider',
+    function($routeProvider,$locationProvider) { 
+        
+        $routeProvider
+        .when('/',{
+        	templateUrl : 'app/views/vue1.html',
+        	controller : "myCtrl"
+        })
+
+        .when("/page2",{
+        	templateUrl : 'app/views/vue2.html',
+        	controller : "myCtrl2"
+        })
+        .otherwise({
+            redirectTo: '/',
+
+        });
+    }
+]);
